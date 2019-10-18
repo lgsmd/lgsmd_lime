ef66dc4178abf2a7ef3b47e9bf8dcae1a916c4ce
lgsmd <584588192@qq.com>
2019-10-18 15:56:53 +0800

    init project

---
+++ src/pages/signin/Signin.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="wrapper">
+    <div class="center">
+      <div class="img">
+        <img :src="imgUrl">
+      </div>
+      <h1 class="name">Sign In</h1>
+      <div class="input-wrapper">
+        <md-field class="input">
+          <md-input :placeholder="'Mobile'"/>
+        </md-field>
+        <md-field  class="input">
+          <md-input :placeholder="'Password'"/>
+        </md-field>
+        <md-button class="button">Sign In</md-button>
+        <text-field></text-field>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  data () {
+    return {
+      imgUrl: '@/assets/imgs/music_512_512.png'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    display: flex
+    flex_direction: column
+    justify-content: center
+    position: fixed
+    left: 0
+    right: 0
+    bottom: 0
+    top: 0
+    .center
+      position : relative
+      text-align:center
+      margin-top: .5rem
+      width: 5rem
+      .img
+        position : absolute
+        padding: 1.6rem
+        top : 0
+        bottom : 0
+        left : 0
+        right : 0
+      .name
+        position : absolute
+        top: 4rem
+        bottom : 0
+        left : 0
+        right : 0
+        font-size: .7rem
+      .input-wrapper
+        position : absolute
+        top: 5.5rem
+        width: 100%
+        .button
+          background: #eee
+</style>
